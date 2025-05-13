daca9a1e86eb8102e3691ed1673bc653d83eb381
Demon <rooskoetv@gmail.com>
2025-05-13 22:07:54 +0300

    Добавление структуры проекта

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps.accounts.apps.AccountsConfig',
+    'apps.profiles.apps.ProfilesConfig',
+    'apps.sellers.apps.SellersConfig',
+    'apps.shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
